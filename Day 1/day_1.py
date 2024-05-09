ee84c0f7c5dab8f9aec701d69bef8a7ff35a39a5
StewieGriffin17 <anowarul.asif@gmail.com>
2024-05-10 03:03:37 +0600

    Day 1 complete

---
+++ Day 1/day_1.py
@@ -0,0 +1,17 @@
+# Basics of print statement and string manipulation
+print("Hello World!")
+print('Hello "World"')
+print("Hello\nWorld")
+print("Hello" + " " + "Everyone")
+
+# Introduction to input function
+print("Hello " + input("What's your name? ") + "!")
+
+# Variables
+year = 2024
+birth_year = int(input("What's your birth year? "))
+age = year - birth_year
+print("Approximate Age: ", age, "years")
+location = input("Where are you from? ")
+length = len(location)
+print(length)
